b5142a55653b8ad9b726e3e801e190b4fb69d3d1
Guilherme Rodrigues <guilherme.s.rodrigues16@aluno.senai.br>
2026-09-09 21:45:30 -0300

    Aula 06

--- src/main/java/com/example/imagemPecas/domain/Service/ImageService.java
+++ src/main/java/com/example/imagemPecas/domain/Service/ImageService.java
@@ -2,6 +2,10 @@ package com.example.imagemPecas.domain.Service;
 
 import com.example.imagemPecas.domain.entity.Image;
 
+import java.util.Optional;
+
 public interface ImageService {
     Image save (Image image);
+
+    Optional<Image> getById(String id);
 }
